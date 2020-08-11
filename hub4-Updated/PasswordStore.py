0ff12c31e02b72e04a90166c161b2d0d4ca43a64
Raman Raja <ramanraja@users.noreply.github.com>
2020-08-11 19:28:22 +0530

    tests added
    This is very buggy code; do not use it on aircraft or submarines !

---
+++ hub4-Updated/PasswordStore.py
@@ -0,0 +1,27 @@
+# secret file storing encryption password and salt
+
+'''
+# local MariaDB redentials
+db_user = 'xxxx'
+db_password = 'xxxx'
+db_name = 'xxxx'
+db_host = 'xxxx'
+db_port = 1234
+'''
+
+# Amazon MqSql credentials
+db_user='xxxx' 
+db_password='xxxxx' 
+db_name = 'xxx'
+db_host = 'xxxx'
+db_port = 1234
+
+# to encrypt config_item field in AppConfig table
+app_config_password = "xxxx"  
+app_config_salt = "xxxx"          
+
+# To generate JWT tokens
+hub_id = "xxxx"
+jwt_algorithm = 'xxx'
+jwt_secret = 'xxxx'
+jwt_life_time = 7        # in days
